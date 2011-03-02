5467d7078ff0810f22e6935cae5bc185779d564e
Tha.Suresh <suresh@mercury.labs.slashprog.com>
2011-03-02 17:50:58 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nanbenda::Application.config.secret_token = '63fc6c12c5cee331faf95ab63102b6191c96f88d098795d6f784fc8c215f904a5b5eba6ded8905b65246a5d2e6d3455b19339242916cb16fac54209df164e954'
